ea9d2fbaa3016baf9e4be21c851c3a10af148bae
張雅茹 <3a832060@gm.student.ncut.edu.tw>
2021-11-29 05:32:54 +0800

    頁面名稱和超連結 修改Route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[HomeController::class,'index'])->get('home.index');
-Route::get('/about',[HomeController::class,'index'])->get('about.index');
-Route::get('/news',[HomeController::class,'index'])->get('news.index');
+
+Route::get('/',[HomeController::class,'index'])->name('home.index');
+Route::get('/about',[AboutController::class,'index'])->name('about.index');
+Route::get('/news',[NewsController::class,'index'])->name('news.index');
